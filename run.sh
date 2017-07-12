73eeaf3a635eb64baba68c347c3be8906ae1d510
hikalium <hikalium@hikalium.com>
2017-07-12 22:52:48 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-URL=https://step-reversi.appspot.com
 BLACK=HUMAN
 WHITE=https://othello-greedy.appspot.com
+#
+URL=https://step-reversi.appspot.com
 VIEWERPATH=`curl "$URL/new?black=$BLACK&white=$HUMAN" | sed -e 's/.*"\(.*\)".*/\1/g'`
 VIEWERURL=$URL$VIEWERPATH
 REFLECTOR_ARGS=`curl $VIEWERURL | grep go | sed -e 's/.*"\go run reflector.go \(.*\)".*/\1/g'`
